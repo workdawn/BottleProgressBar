723e074471bf28c595b80cc72aadc878489838d8
hys_zion <miaowei@mail.haoyisheng.com>
2017-08-01 17:20:31 +0800

    fix bug

--- app/src/main/java/com/views/bottleprogressbar/widget/BottleProgressBar.java
+++ app/src/main/java/com/views/bottleprogressbar/widget/BottleProgressBar.java
@@ -453,6 +453,7 @@ public class BottleProgressBar extends ProgressBar {
                 }
                 wavePath.lineTo(mWidth, mHeight);
                 wavePath.lineTo(0, mHeight);
+                wavePath.lineTo(circleLeftCenterX, points[1]);
                 wavePath.close();
                 canvas.drawPath(wavePath, mWaterPaint);
                 if(animator != null && animator.isPaused()){
